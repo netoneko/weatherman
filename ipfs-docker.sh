cc6c902f1b5d77dd3540b9a3486362dde4e85fde
Kirill Maksimov <kirill@orbs.com>
2020-02-15 19:11:37 -0700

    Update ipfs config

--- ipfs-docker.sh
+++ ipfs-docker.sh
@@ -2,5 +2,5 @@
 
 docker run -d --name ipfs-node \
   -v /opt/ipfs-docker-staging:/export -v /opt/ipfs-docker-data:/data/ipfs \
-  -p 8080:8080 -p 4001:4001 -p 127.0.0.1:5001:5001 \
+  -p 8080:8080 -p 4001:4001 -p 5001:5001 \
   ipfs/go-ipfs:latest
