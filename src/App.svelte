b8ea5569f09c9fe9fa3b079a54418b5b0e815b55
Kirill Maksimov <kirill@orbs.com>
2020-02-16 00:59:21 -0700

    UI

--- src/App.svelte
+++ src/App.svelte
@@ -78,7 +78,7 @@ a {
 <div class="centered">
 <h2>Weather in Denver for the next 12 hours</h2>
 {#if updatedAt}
-<p>Last updated {lastUpdatedAt()}</p>
+<p>Last updated <a href="{sourceURL(hash)}" target="_blank" title="raw data">{lastUpdatedAt()}</a></p>
 {/if}
 {#if isEmpty(forecasts)}
 <p>Loading...</p>
@@ -106,7 +106,6 @@ a {
 <p>Powered by IPFS oracle on <a href="https://orbs.com">ORBS</a>:
 <a href="https://github.com/orbs-network/orbs-network-go/compare/experimental/denver-hackathon#diff-caf51ea546fd78b33ed5849164bde68e" target="_blank">blockchain node changes</a>,
 <a href="https://github.com/netoneko/weatherman/blob/master/src/worker.js" target="_blank">oracle worker source</a>,
-<a href="https://github.com/netoneko/weatherman/blob/master/src/contract/weatherman/contract.go" target="_blank">smart contract source</a>,
-<a href="{sourceURL(hash)}" target="_blank">raw data</a>
+<a href="https://github.com/netoneko/weatherman/blob/master/src/contract/weatherman/contract.go" target="_blank">smart contract source</a>
 </p>
 </div>
